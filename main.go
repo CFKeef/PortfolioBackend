bc9c07951a95970e8e0a1f7493fb39b5ef0706a1
CFKeef <PatryckG@outlook.com>
2021-11-14 14:47:27 -0500

    Fix

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"github.com/joho/godotenv"
-	"log"
 	"portfolioBE/integrations"
 	"portfolioBE/server"
 )
 
 func init() {
-	err := godotenv.Load(".env")
+	godotenv.Load(".env")
 
 	integrations.SetUp()
 }
